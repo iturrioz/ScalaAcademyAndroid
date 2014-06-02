903f45d063588ba2148e30aa6b9953eee08a8b73
Kepa Iturrioz <kepa.iturrioz@gmail.com>
2014-06-02 23:14:28 +0200

    removed TypedActivity

--- src/main/scala/MainActivity.scala
+++ src/main/scala/MainActivity.scala
@@ -3,7 +3,7 @@ package net.iturrioz
 import _root_.android.app.Activity
 import _root_.android.os.Bundle
 
-class MainActivity extends Activity with TypedActivity {
+class MainActivity extends Activity {
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
     setContentView(R.layout.main)
