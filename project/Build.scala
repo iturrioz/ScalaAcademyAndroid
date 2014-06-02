6023bb00a2a14dad201bdf406f7006a7b585503e
Kepa Iturrioz <kepa.iturrioz@gmail.com>
2014-06-02 23:07:08 +0200

    template

---
+++ project/Build.scala
@@ -0,0 +1,38 @@
+import sbt._
+
+import Keys._
+import AndroidKeys._
+
+object General {
+  val settings = Defaults.defaultSettings ++ Seq (
+    name := "ScalaAcademyAndroid",
+    version := "0.1",
+    versionCode := 0,
+    scalaVersion := "2.10.1",
+    platformName in Android := "android-19",
+    javacOptions ++= Seq("-encoding", "UTF-8", "-source", "1.6", "-target", "1.6")
+  )
+
+  val proguardSettings = Seq (
+    useProguard in Android := true
+  )
+
+  lazy val fullAndroidSettings =
+    General.settings ++
+    AndroidProject.androidSettings ++
+    TypedResources.settings ++
+    proguardSettings ++
+    AndroidManifestGenerator.settings ++
+    AndroidMarketPublish.settings ++ Seq (
+      keyalias in Android := "change-me",
+      libraryDependencies += "org.scalatest" %% "scalatest" % "2.1.7" % "test"
+    )
+}
+
+object AndroidBuild extends Build {
+  lazy val main = Project (
+    "ScalaAcademyAndroid",
+    file("."),
+    settings = General.fullAndroidSettings
+  )
+}
---
+++ src/main/scala/MainActivity.scala
@@ -0,0 +1,11 @@
+package net.iturrioz
+
+import _root_.android.app.Activity
+import _root_.android.os.Bundle
+
+class MainActivity extends Activity with TypedActivity {
+  override def onCreate(bundle: Bundle) {
+    super.onCreate(bundle)
+    setContentView(R.layout.main)
+  }
+}
