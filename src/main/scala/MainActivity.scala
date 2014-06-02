441a29d22aede5859cb1827a9bff19309e4f62f9
Kepa Iturrioz <kepa.iturrioz@gmail.com>
2014-06-03 00:08:34 +0200

    added text and a listener

--- src/main/scala/MainActivity.scala
+++ src/main/scala/MainActivity.scala
@@ -1,11 +1,32 @@
 package net.iturrioz
 
-import _root_.android.app.Activity
-import _root_.android.os.Bundle
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
-class MainActivity extends Activity {
+class MainActivity extends Activity with TypedActivity with MainActivity.Listeners {
+
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
     setContentView(R.layout.main)
+
+    val textview: TextView = findView(TR.textview)
+
+    textview.setText("Hi")
+    textview.setOnClickListener((v: View) => textview.setText("I'm still here"))
+  }
+}
+
+object MainActivity {
+
+  trait Listeners {
+    implicit def function2ViewOnClickListener(f: View => Unit) : View.OnClickListener = {
+      new View.OnClickListener() {
+        def onClick(view: View) {
+          f(view)
+        }
+      }
+    }
   }
 }
