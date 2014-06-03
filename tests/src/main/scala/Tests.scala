aa2820ce4bc95c057a741a085434b9e196fd70d0
Kepa Iturrioz <kepa.iturrioz@gmail.com>
2014-06-03 23:08:16 +0200

    test content

--- tests/src/main/scala/Tests.scala
+++ tests/src/main/scala/Tests.scala
@@ -2,13 +2,21 @@ package net.iturrioz.tests
 
 import net.iturrioz._
 import junit.framework.Assert._
-import _root_.android.test.AndroidTestCase
-import _root_.android.test.ActivityInstrumentationTestCase2
+import android.test.{TouchUtils, AndroidTestCase, ActivityInstrumentationTestCase2}
 
 class AndroidTests extends AndroidTestCase {
-
+  def testPackageIsCorrect() {
+    assertEquals("net.iturrioz", getContext.getPackageName)
+  }
 }
 
 class ActivityTests extends ActivityInstrumentationTestCase2(classOf[MainActivity]) {
+  def testHelloWorldIsShown() {
+    val activity = getActivity
+    val textview = activity.findView(TR.textview)
+    assertEquals(textview.getText, "Hi")
 
+    TouchUtils.clickView(this, textview)
+    assertEquals(textview.getText, "I'm still here")
+  }
 }
